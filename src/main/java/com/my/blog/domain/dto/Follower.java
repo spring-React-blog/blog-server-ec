2e98682d2200ae9e0db9ca482f61c5db97fdfaad
chong <chd830@naver.com>
2022-06-03 16:21:52 +0900

    add Follower table

---
+++ src/main/java/com/my/blog/domain/dto/Follower.java
@@ -0,0 +1,27 @@
+package com.my.blog.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "Followers")
+public class Follower {
+
+    @Id
+    @Column(name = "follwer_id")
+    private long followerId;
+
+    @Column(name = "from_member_id")
+    private long fromMemberId;
+
+    @Column(name = "to_member_id")
+    private long toMemberId;
+
+}
