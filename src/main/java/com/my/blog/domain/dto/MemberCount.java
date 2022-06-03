668538af361debce38ab5aef24268f467ec77811
chong <chd830@naver.com>
2022-06-03 16:08:41 +0900

    add MemberCount table

---
+++ src/main/java/com/my/blog/domain/dto/MemberCount.java
@@ -0,0 +1,32 @@
+package com.my.blog.domain.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "MemberCounts")
+public class MemberCount {
+
+    @Id
+    @Column(name = "member_count_id")
+    private int memberCountId;
+
+    @Column(name = "follower_count")
+    private int followerCount;
+
+    @Column(name = "following_count")
+    private int followingCount;
+
+    @Column(name = "board_count")
+    private int boardCount;
+
+    @OneToOne(mappedBy = "memberCountId")
+    private Member member;
+
+
+}
