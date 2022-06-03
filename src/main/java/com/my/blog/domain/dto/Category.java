eb387cf4749be018c802fe861f2e849e7ea52822
chong <chd830@naver.com>
2022-06-03 16:21:38 +0900

    add Category table

---
+++ src/main/java/com/my/blog/domain/dto/Category.java
@@ -0,0 +1,25 @@
+package com.my.blog.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "Categories")
+public class Category {
+
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "member_id")
+    @OneToOne(mappedBy = "member_id")
+    private Member memberId;
+}
