7aca619c525ae4020a27a9a3e6d4457c6d180451
chong <chd830@naver.com>
2022-06-03 16:20:59 +0900

    add Board table

---
+++ src/main/java/com/my/blog/domain/dto/Board.java
@@ -0,0 +1,44 @@
+package com.my.blog.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "Boards")
+public class Board {
+
+    @Id
+    @Column(name = "board_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "board_img")
+    private String boardImg;
+
+    @Column(name = "open_status")
+    private boolean openStatus;
+
+    @Column(name = "board_count_id")
+    @OneToOne(mappedBy = "member_id")
+    private Member member;
+
+    @Column(name = "board_count_id")
+    @OneToOne(mappedBy = "board_count_id")
+    private Board boardCountId;
+
+    @Column(name = "category_id")
+    @OneToOne(mappedBy = "category_id")
+    private Category categoryId;
+
+
+}
